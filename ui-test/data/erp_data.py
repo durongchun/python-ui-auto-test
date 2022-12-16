0b1f35a08bf2c56f05463a0f489fec61d22f77e1
lucy <lucy.du@holinova.com>
2022-12-16 12:32:15 -0800

    change dev url

--- ui-test/data/erp_data.py
+++ ui-test/data/erp_data.py
@@ -42,7 +42,7 @@ class ErpData:
     test_make_orders_data = PageCommon.test_convert_data(
         ExcelReader.get_test_make_orders_xls(PageCommon.get_data_path()))
 
-    url = "https://dev.mtboucherie.com/web#cids=1"
+    url = "http://192.168.99.37:8069/web/login"
     user_name = "lucy.du@holinova.com"
     pass_word = "durongchun123~"
     product_url = "https://dev.mtboucherie.com/web#menu_id=243&action=390&model=product.template&view_type=kanban"
