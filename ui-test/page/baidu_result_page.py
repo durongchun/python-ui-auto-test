02ea607576bd8754adfbec03084a7bccf27eb758
durongchun <durongchun@hotmail.com>
2022-12-21 13:08:54 -0800

    add waiting

--- ui-test/page/baidu_result_page.py
+++ ui-test/page/baidu_result_page.py
@@ -6,4 +6,5 @@ from locator.baidu_result_locator import BaiduResultLocator
 class BaiduResultPage(PageCommon):
     # 百度搜索结果页点击第一行链接操作
     def click_first_link(self):
+
         self.click_element(BaiduResultLocator.link_a)
